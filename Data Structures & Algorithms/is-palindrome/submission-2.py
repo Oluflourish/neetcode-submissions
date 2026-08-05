6fea7af0a9356f3df8b5a338d25d8317a44c54ce
Olu-Flourish Emmanuel <flourishisnow@gmail.com>
2026-08-04 20:33:45 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,20 @@
+import string
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        lower = list(string.ascii_lowercase)
+        upper = list(string.ascii_uppercase)
+        nums = list('0123456789')
+        
+        word = []
+        for c in s:
+            if c in lower or c in upper or c in nums:
+                word.append(c.lower())
+
+        text = ''.join(word)
+
+        if text == text[::-1]:
+            return True
+
+        return False
+
+        
